c2d377819ba5d03fef457af3d0f1fc12e65c7b6c
Michaux Kelley <michauxkelley@gmail.com>
2019-01-13 23:00:27 -0500

    Add initial scaffolding

---
+++ src/components/Schedulizer.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <header>
+      <h1>Schedulizer</h1>
+      <table>
+        <tr>
+          <th>Cron</th>
+          <th>Name</th>
+        </tr>
+        <tr v-for="task in tasks" :key='task.id'>
+          <td class="data-table-cell">{{task.attributes.cron}}</td>
+          <td class="data-table-cell">{{task.attributes.name}}</td>
+        </tr>
+      </table>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Schedulizer',
+  data() {
+    return {
+      tasks: []
+    };
+  },
+  created() {
+    window
+      .fetch('https://scheduler-challenge.herokuapp.com/schedule')
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        console.log(json.data);
+        this.tasks = json.data;
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+.data-table-cell {
+  text-align: left;
+}
+</style>
